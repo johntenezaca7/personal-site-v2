7e8d88586c8be13a8a9b8b42e7440f7e70ce6c76
Owner <owner@MacBook-Pro-5.local>
2018-08-28 00:04:48 -0400

    added nav bar and desktop pic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+const appRoutes: Routes = [{ path: "", component: AppComponent }];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NavBarComponent],
+  imports: [BrowserModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
